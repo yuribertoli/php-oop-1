83d0157698c7f32e1aad0feb090e87be8875178b
yuribertoli <yuribertoli@yahoo.it>
2022-03-15 17:26:08 +0100

    extra

--- index.php
+++ index.php
@@ -29,6 +29,61 @@ class Movie {
     }
 }
 
+foreach($film as $key => $value){
+
+    $nome_array = "$".$key;
+    $nome_array = new Movie($value["title"], $value["genre"], $value["cast"]);
+    $nome_array->generi = $value["genre"];
+    $nome_array->attori = $value["cast"];
+    $nome_array->regista = $value["director"];
+    $nome_array->annoProduzione = $value["year_of_production"];
+    $nome_array->durata = $value["movie_length"];
+    $nome_array->punteggio = $value["rating"];
+    $nome_array->origine($value["country"], $value["language"]);
+
+    //stampo a schermo i valori
+    echo "<strong>Titolo:</strong> $nome_array->titolo <br>";
+
+    /* ciclo i valori di generi e aggiungo la virgola solo se l'elemento ciclato non è l'ultimo */
+    echo "<strong>Generi: </strong>";
+    $i=0;
+    foreach($nome_array->generi as $key => $genere){
+        $i++;
+        if($i < count($nome_array->generi)){
+            echo $genere . ", ";
+        } else {
+            echo $genere;
+        }
+    }
+    echo "<br>";
+
+    /* ripeto la stessa operazione per attori */
+    echo "<strong>Attori: </strong>";
+    $i=0;
+    foreach($nome_array->attori as $key => $genere){
+        $i++;
+        if($i < count($nome_array->attori)){
+            echo $genere . ", ";
+        } else {
+            echo $genere;
+        }
+    }
+    echo "<br>";
+
+    echo "<strong>Regista:</strong> $nome_array->regista <br>";
+    echo "<strong>Anno di produzione:</strong> $nome_array->annoProduzione <br>";
+    echo "<strong>Durata del film:</strong> $nome_array->durata <br>";
+    echo "<strong>Punteggio (da 0 a 10):</strong> $nome_array->punteggio <br>";
+    echo "<strong>Produzione:</strong> {$nome_array->origine($value["country"], $value["language"])} <br><br><br>";
+
+}
+
+
+
+
+/* 
+
+
 //definisco le variabili da cui creo il percorso per i film
 $film1 = $film['Underground'];
 $film2 = $film['Film d\'amore e d\'anarchia'];
@@ -48,7 +103,7 @@ $underground->origine($film1["country"], $film1["language"]);
 //stampo a schermo i valori
 echo "<strong>Titolo:</strong> $underground->titolo <br>";
 
-/* ciclo i valori di generi e aggiungo la virgola solo se l'elemento ciclato non è l'ultimo */
+// ciclo i valori di generi e aggiungo la virgola solo se l'elemento ciclato non è l'ultimo 
 echo "<strong>Generi: </strong>";
 $i=0;
 foreach($underground->generi as $key => $genere){
@@ -61,7 +116,7 @@ foreach($underground->generi as $key => $genere){
 }
 echo "<br>";
 
-/* ripeto la stessa operazione per attori */
+// ripeto la stessa operazione per attori 
 echo "<strong>Attori: </strong>";
 $i=0;
 foreach($underground->attori as $key => $genere){
@@ -81,9 +136,9 @@ echo "<strong>Punteggio (da 0 a 10):</strong> $underground->punteggio <br>";
 echo "<strong>Produzione:</strong> {$underground->origine($film1["country"], $film1["language"])} <br><br><br>";
 
 
+ */
 
-
-
+/* 
 
 //prendo il nome del titolo definito nell'array ricercato nel database
 $anarchia = new Movie($film2["title"], $film2["genre"], $film2["cast"]);
@@ -100,7 +155,7 @@ $anarchia->origine($film2["country"], $film2["language"]);
 //stampo a schermo i valori
 echo "<strong>Titolo:</strong> $anarchia->titolo <br>";
 
-/* ciclo i valori di generi e aggiungo la virgola solo se l'elemento ciclato non è l'ultimo */
+// ciclo i valori di generi e aggiungo la virgola solo se l'elemento ciclato non è l'ultimo 
 echo "<strong>Generi: </strong>";
 $i=0;
 foreach($anarchia->generi as $key => $genere){
@@ -113,7 +168,7 @@ foreach($anarchia->generi as $key => $genere){
 }
 echo "<br>";
 
-/* ripeto la stessa operazione per attori */
+// ripeto la stessa operazione per attori 
 echo "<strong>Attori: </strong>";
 $i=0;
 foreach($anarchia->attori as $key => $genere){
@@ -131,3 +186,4 @@ echo "<strong>Anno di produzione:</strong> $anarchia->annoProduzione <br>";
 echo "<strong>Durata del film:</strong> $anarchia->durata <br>";
 echo "<strong>Punteggio (da 0 a 10):</strong> $anarchia->punteggio <br>";
 echo "<strong>Produzione:</strong> {$anarchia->origine($film2["country"], $film2["language"])} <br>";
+ */
